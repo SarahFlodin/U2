8423650e9770b4858aca582b5df53e9c86274087
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-03-02 12:54:41 +0100

    HTML element from JS to HTML
    Render a dessert object into HTML element

--- main.js
+++ main.js
@@ -31,3 +31,68 @@ function removeDessertbyId(desserts, id) {
         }
     }
 }
+
+// Returnerar desserten baserat på deras smak "flavor"
+function getDessertsByFlavor (desserts, flavor) {
+    let dessertsByFlavor = [];
+//Skapar en if loop så det inte spelar roll om små eller stora bokstäver används och matchar med smaken
+    for (let desserts of dessert) {
+        if (dessert.flavor.toLowerCase() == flavor.toLowerCase()) {
+            dessertsByFlavor.push(dessert);
+        }
+    }
+    return dessertsByFlavor;
+}
+
+// Returnerar desserten baserat på deras kalorier
+function getDessertsByKalories (desserts, kalories) {
+    let dessertsByKalories = [];
+//Skapar en if loop så det inte spelar roll om små eller stora bokstäver används och matchar med smaken
+    for (let desserts of dessert) {
+        if (dessert.kalories == kalories) {
+            dessertsByKalories.push(dessert);
+        }
+    }
+    return dessertsByKalories;
+}
+
+// Returnerar desserten baserat på deras form "shape"
+function getDessertsByShape (desserts, shape) {
+    let dessertsByShape = [];
+//Skapar en if loop så det inte spelar roll om små eller stora bokstäver används och matchar med smaken
+    for (let desserts of dessert) {
+        if (dessert.shape.toLowerCase() == flavor.toLowerCase()) {
+            dessertsByShape.push(dessert);
+        }
+    }
+    return dessertsByShape;
+}
+
+// Returnerar desserten baserat på om dem är glutenfria
+function getDessertsByFlavor (desserts, glutenfree) {
+    let dessertsByGlutenfree = [];
+//Skapar en if loop så det inte spelar roll om små eller stora bokstäver används och matchar med smaken
+    for (let desserts of dessert) {
+        if (dessert.glutenfree.toLowerCase() == glutenfree.toLowerCase()) {
+            dessertsByGlutenfree.push(dessert);
+        }
+    }
+    return dessertsByGlutenfree;
+}
+
+//Skapar en dessert objekt till HTML element
+function renderDessert(dessert) {
+    let div = document.createElement("div");
+    div.classList.add("dessert");
+    div.id = dessert.id;
+    div.innerHTML = `
+        <div>${dessert.name}</div>
+        <div>${dessert.flavor}</div>
+        <div>${dessert.kalories}</div>
+        <div>${dessert.shape}</div>
+        <div>${dessert.glutenfree}</div>
+        <button type="button"> Remove </button>
+        `;
+
+    return div;
+}
