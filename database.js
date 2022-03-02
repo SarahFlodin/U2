e3926a7ce1eeda387e78f8366a7a84ce1da29ea9
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-03-02 12:30:49 +0100

    Database over desserts done
    10 desserts database done

---
+++ database.js
@@ -0,0 +1,91 @@
+"use strict"
+let database = [
+    {
+        id: 1,
+        name: "Chocolate cake",
+        flavor: "Chocolate",
+        kalories: 1520,
+        shape: "Round",
+        glutenfree: "No"
+    }
+
+    {
+        id: 2,
+        name: "Semla",
+        flavor: "Almond",
+        kalories: 500,
+        shape: "Round",
+        glutenfree: "No"
+    }
+
+    {
+        id: 3,
+        name: "Delicato ball",
+        flavor: "Chocolate",
+        kalories: 278,
+        shape: "Round",
+        glutenfree: "Yes"
+    }
+
+    {
+        id: 4,
+        name: "Pannacotta",
+        flavor: "Raspberry",
+        kalories: 354,
+        shape: "Round",
+        glutenfree: "Yes"
+    }
+
+    {
+        id: 5,
+        name: "Princess cake",
+        flavor: "Almond",
+        kalories: 1720,
+        shape: "Round",
+        glutenfree: "No"
+    }
+
+    {
+        id: 6,
+        name: "Cinnamon bun",
+        flavor: "Cinnamon",
+        kalories: 286,
+        shape: "Round",
+        glutenfree: "No"
+    }
+
+    {
+        id: 7,
+        name: "Raspberry caves",
+        flavor: "Raspberry",
+        kalories: 130,
+        shape: "Round",
+        glutenfree: "No"
+    }
+
+    {
+        id: 8,
+        name: "Brownies",
+        flavor: "Chocolate",
+        kalories: 683,
+        shape: "Squere",
+        glutenfree: "No"
+    }
+
+    {
+        id: 9,
+        name: "Chessboard cookies ",
+        flavor: "Chocolate",
+        kalories: 234,
+        shape: "Squere",
+        glutenfree: "No"
+    }
+    {
+        id: 10,
+        name: "Banoffie",
+        flavor: "Banana",
+        kalories: 439,
+        shape: "Round",
+        glutenfree: "Yes"
+    }
+]
